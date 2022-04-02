49779f74b5787c6f22754d847e2024de02493c09
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-04-02 14:27:43 +0500

    window creation

---
+++ arfhbadfb.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <GL/freeglut.h>
+
+void RenderSceneGB() {
+    glClearColor(0.0f, 0.3f, 0.5f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("IDKWTD");
+
+    glutDisplayFunc(RenderSceneGB);
+    glutMainLoop();
+}
