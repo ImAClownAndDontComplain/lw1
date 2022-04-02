66b5a0ec478f4ff5dfa2de29d99d1e49ff1d00ee
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-04-02 16:24:29 +0500

    разбираться с функциями

--- arfhbadfb.cpp
+++ arfhbadfb.cpp
@@ -7,7 +7,7 @@ void RenderSceneGB() {
     glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawArrays(GL_POINTS, 3, 2);
+    //glDrawArrays(GL_POINTS, 3, 2);
     glutSwapBuffers();
 }
 GLuint VBO;
@@ -40,19 +40,28 @@ int main(int argc, char **argv)
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);*/
 
-    glm::vec3 TriangleWithPoints[5] = {
+   /* glm::vec3 TriangleWithPoints[5] = {
         {-0.8f, -0.8f, 0.0f},
         {0.8f, -0.8f, 0.0f},
         {0.0f, -0.2f, 0.0f},
         {0.0f, 0.0f, 0.0f},
         {0.0f, 0.2f, 0.0f}
+    };*/
+    glm::vec3 Triangle[3] = {
+       {-0.8f, -0.8f, 0.0f},
+       {0.8f, -0.8f, 0.0f},
+       {0.0f, -0.2f, 0.0f}
+    };
+    glm::vec3 Points[2]{
+       {0.0f, 0.0f, 0.0f},
+       { 0.0f, 0.2f, 0.0f }
     };
     GLuint buf[2];
     buf[0] = VBO;
     buf[1] = AAA;
     glGenBuffers(2, buf);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleWithPoints), TriangleWithPoints, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle), Triangle, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     //glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
