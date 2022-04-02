0d1341544558942ac1b429f54061b9e457efe53a
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-04-02 15:15:22 +0500

    точка не отрисовывается но я переживу

--- arfhbadfb.cpp
+++ arfhbadfb.cpp
@@ -1,8 +1,10 @@
-﻿#include <iostream>
+﻿#include <GL/glew.h>
 #include <GL/freeglut.h>
+#include "glm/vec3.hpp"
+#include <iostream>
 
 void RenderSceneGB() {
-    glClearColor(0.0f, 0.3f, 0.5f, 0.0f);
+    glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers();
 }
@@ -17,5 +19,27 @@ int main(int argc, char **argv)
     glutCreateWindow("IDKWTD");
 
     glutDisplayFunc(RenderSceneGB);
+
     glutMainLoop();
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glm::vec3 Vertices[1];
+    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+    
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_POINTS, 0, 1);
+
 }
