1794042ec9dfd4d5cd8b9ff06f9497ccbe55e83c
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-04-02 15:47:02 +0500

    triangle with two points

--- arfhbadfb.cpp
+++ arfhbadfb.cpp
@@ -6,7 +6,8 @@
 void RenderSceneGB() {
     glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    glDrawArrays(GL_POINTS, 0, 1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_POINTS, 3, 2);
     glutSwapBuffers();
 }
 
@@ -27,9 +28,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    glm::vec3 Vertices[1];
+    /*glm::vec3 Vertices[1];
     Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
-    
+
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
@@ -37,6 +38,21 @@ int main(int argc, char **argv)
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);*/
+
+    glm::vec3 TriangleWithPoints[5] = {
+        {-0.8f, -0.8f, 0.0f},
+        {0.8f, -0.8f, 0.0f},
+        {0.0f, -0.2f, 0.0f},
+        {0.0f, 0.0f, 0.0f},
+        {0.0f, 0.2f, 0.0f}
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleWithPoints), TriangleWithPoints, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
     glutDisplayFunc(RenderSceneGB);
